18db2a11d6eab55e602477f045ce23710e0c0ca5
Suryanshu <suryanshu.1520@gmail.com>
2026-06-23 01:55:25 +0530

    chore(deploy): ignore .next directory and remove from index

--- .next/server/chunks/ssr/_next-internal_server_app_preview_[hash]_page_actions_1ioilbr.js
+++
@@ -1,3 +0,0 @@
-module.exports=[45297,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_preview_%5Bhash%5D_page_actions_1ioilbr.js.map
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"202G2ROMBpKNJOWBUcz7gHWDc+YLf04bfqPm47yAFO4=\"\n}"
